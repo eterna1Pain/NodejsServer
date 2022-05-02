6066a74211e89cbf299e0823ea9313ae5bfdecb4
eterna1_pain <sparta3571@gmail.com>
2022-05-02 19:39:55 +0500

    Test ver

--- server.js
+++ server.js
@@ -1,8 +1,43 @@
 'use strict';
-var http = require('http');
+const http = require('http')
 var port = process.env.PORT || 3008;
-
+let requestsCount = 0
 http.createServer(function ReqRes (request, respond) {
     respond.writeHead(200, { 'Content-Type': 'text/plain' });
-    respond.end('Hello World\n');
+    requestsCount++
+    switch (request.url) {
+        case '/students':
+            respond.write('STUDENTS\n')
+            break;
+        case '/courses':
+            respond.write('FRONT + BACK\n')
+            break;
+        default:
+            respond.write('NOT FOUND\n')
+    }
+    respond.write(' IT KAMASUTRA: ' + requestsCount )
+    respond.end('\nEnd of page\n')
 }).listen(port);
+
+
+
+/*
+let requestsCount = 0
+const server = http.createServer((request, response) => {
+    requestsCount++
+    switch (request.url) {
+        case '/students':
+            response.write('STUDENTS')
+            break;
+        case '/courses':
+            response.write('FRONT + BACK')
+            break;
+        default:
+            response.write('NOT FOUND')
+    }
+    response.write(' IT KAMASUTRA: ' + requestsCount)
+
+    response.end()
+})
+
+server.listen(3003)*/
